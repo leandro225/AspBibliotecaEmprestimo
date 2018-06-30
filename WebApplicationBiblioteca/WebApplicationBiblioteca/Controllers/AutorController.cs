47e43b2aacaa9875cefcf5902833c2853724475b
User <User@DESKTOP-GN27T58>
2018-06-30 09:03:54 -0300

    Criando autor

---
+++ WebApplicationBiblioteca/WebApplicationBiblioteca/Controllers/AutorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationBiblioteca.Controllers
+{
+    public class AutorController : Controller
+    {
+        // GET: Autor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
