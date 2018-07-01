1c7387236e3bd3ce21a825eb83e66e9baf98f6fd
User <User@DESKTOP-GN27T58>
2018-07-01 10:37:55 -0300

    Criando model emprestimo e controller emprestimo

---
+++ WebApplicationBiblioteca/WebApplicationBiblioteca/Controllers/EmprestimoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationBiblioteca.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        // GET: Emprestimo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplicationBiblioteca/WebApplicationBiblioteca/Models/Emprestimo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplicationBiblioteca.Models
+{
+    public class Emprestimo
+    {
+        public int EmprestimoId { get; set; }
+       
+        
+
+      
+        public string PessoaEmprestimo { get; set; }
+
+        [Display(Name = "Data do Empréstimo")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataEmprestimo { get; set; }
+
+        [Display(Name = "Data da Devolução")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataDevolucao { get; set; }
+
+
+        [Display(Name = "Livro")]
+        public int LivroId { get; set; }
+        [ForeignKey("LivroId")]
+        public virtual Livro Livro { get; set; }
+    }
+}
